582f9ccdc0193caaea1bf6ce0ccc950dc5735af1
Sackah <yeitsam5@gmail.com>
2024-02-07 08:24:00 +0000

    doc: signal-factory

--- src/app/utils/signal-factory.ts
+++ src/app/utils/signal-factory.ts
@@ -2,7 +2,10 @@ import {signal, WritableSignal} from "@angular/core";
 import {InitialSig} from "../shared/types/main.types";
 import {BackendErrors} from "../shared/types/auth.types";
 
-
+/**
+ * @function newSignal - creates a new signal with initial state <T>
+ * @returns a new signal with the initial state
+ */
 export const newSignal = <T>() => {
   return signal<InitialSig<T>>({
     pending: false,
@@ -11,6 +14,10 @@ export const newSignal = <T>() => {
   })
 }
 
+/**
+ * @function pendSignal - updates a signal to pending state
+ * @param signal - the signal to be updated
+ */
 export const pendSignal = <T extends {}>(signal: WritableSignal<InitialSig<T>>) => {
   signal.set({
     data: null,
@@ -19,6 +26,11 @@ export const pendSignal = <T extends {}>(signal: WritableSignal<InitialSig<T>>) 
   })
 }
 
+/**
+ * @function completeSignal - updates a signal to complete state
+ * @param signal - the signal to be updated
+ * @param data - the data recieved after api call/ process completes
+ */
 export const completeSignal = <T extends {}>(signal: WritableSignal<InitialSig<T>>, data: T) => {
   signal.set({
     data,
@@ -27,6 +39,11 @@ export const completeSignal = <T extends {}>(signal: WritableSignal<InitialSig<T
   })
 }
 
+/**
+ * @function errorSignal - updates a signal to error state
+ * @param signal - the signal to be updated
+ * @param error - the error recieved after api call/ process completes
+ */
 export const errorSignal = <T extends {}>(signal: WritableSignal<InitialSig<T>>, error: BackendErrors) => {
   signal.set({
     data: null,
